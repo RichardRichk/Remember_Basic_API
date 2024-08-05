e8317a9b49a8abcb8f695948241d30a59c01851b
Richard <richardfpassarelli@gmail.com>
2024-08-05 10:40:26 -0300

    Subindo projeto

---
+++ ProductAPI/Contexts/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Domains;
+using System.Buffers.Text;
+
+namespace ProductAPI.Contexts
+{
+    public class Context : DbContext
+    {
+        //Definindo as entidades do banco de dados
+
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=NOTE17-S21\\SQLSERVER; Database=ProductAPI; User Id=sa; Password=Senai@134; TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Interfaces;
+using ProductAPI.Repository;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository { get; set; }
+
+        public ProductController()
+        {
+            _productRepository = new ProductRepository();
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(_productRepository.GetAll());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+    }
+}
